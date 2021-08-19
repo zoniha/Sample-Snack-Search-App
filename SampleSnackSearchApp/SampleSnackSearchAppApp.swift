aeb44e1c0afc63d20dde5e0e8ab5130dee109806
zoniha <awaawapurin@gmail.com>
2021-08-19 21:48:50 +0900

    Initial Commit

---
+++ SampleSnackSearchApp/SampleSnackSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSnackSearchAppApp.swift
+//  SampleSnackSearchApp
+//
+//  Created by zoniha on 2021/08/19.
+//
+
+import SwiftUI
+
+@main
+struct SampleSnackSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
